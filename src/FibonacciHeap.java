8993866ca73b05e20fad7574552aff5b233f56b7
Debdeep Basu <debdeepbasucal@hotmail.com>
2018-11-04 23:31:35 -0500

    Implement file parsing and declare the public apis for FibonacciHeap

--- src/FibonacciHeap.java
+++ src/FibonacciHeap.java
@@ -1,6 +1,31 @@
+import java.util.*;
 
 public class FibonacciHeap {
-	FibonacciHeap() {
-		
+	
+	private HashMap<String, Node> map = new HashMap<String, Node>(); 
+	
+	public void insertOrUpdate(String str, int frequency) {
+		if (map.containsKey(str)) {
+			increaseKey(str, frequency);
+		} else {
+			insert(str, frequency);
+		}
 	}
+	
+	public void getMostUsed(int num) {
+		System.out.println("num:" + num);
+		return;
+	}
+	
+	void insert(String str, int frequency) {
+		return;
+	}
+	
+	void increaseKey(String str, int frequency) {
+		return;
+	}
+}
+
+class Node {
+	
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,39 @@
+import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Main {
 
+	private static Scanner sc;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello");
+		if (args.length == 0) {
+			System.out.println("Please pass file url as a command line argument");
+			return;
+		}
+		File file = new File(args[0]);
+		FibonacciHeap fh = new FibonacciHeap();
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				String line = sc.nextLine();
+				if (line.equals("stop")) return;
+				if (line.startsWith("$")) {
+					String[] words = line.split(" ");
+					fh.insertOrUpdate(words[0].substring(1), Integer.parseInt(words[1]));
+				} else {
+					fh.getMostUsed(Integer.parseInt(line));
+				}
+			}
+			sc.close();
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Please enter a valid file url");
+			e.printStackTrace();
+		}
 	}
 
 }
