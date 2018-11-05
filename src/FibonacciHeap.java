f0f29d1bbd8c1534ec3c385361463b1e7b9c38fb
Debdeep Basu <debdeepbasucal@hotmail.com>
2018-11-05 18:53:15 -0500

    Take the children of maxNode and insert it to the top level circular list.

--- src/FibonacciHeap.java
+++ src/FibonacciHeap.java
@@ -11,13 +11,35 @@ public class FibonacciHeap {
 		if (map.containsKey(str)) {
 			increaseKey(str, frequency);
 		} else {
-			Node node = insert(frequency);
+			Node node = insert(str, frequency);
 			map.put(str, node);
 		}
 	}
 	
-	public void getMostUsed(int num) {
-		return;
+	public ArrayList<String> getMostUsed(int num) {
+		// do deleteMax num times and then reinsert it back into
+		// the fibHeap
+		ArrayList<Node> deletedNodes = new ArrayList<Node>();
+		ArrayList<String> result = new ArrayList<String>();
+		for (int i = 0; i < num; i++) {
+			Node deleted = deleteMax();
+			deletedNodes.add(deleted);
+			result.add(deleted.name);
+		}
+		// insert it back into the fibHeap
+		return result;
+	}
+
+	Node deleteMax() {
+		// take the children of maxNode and insert it to the top level circular list
+		for (Node child : maxNode.children)
+			insert(child);
+		// detach maxNode from the toplevel circular list
+		maxNode.prev.next = maxNode.next;
+		maxNode.next.prev = maxNode.prev;
+		if (head == maxNode) head = maxNode.next;
+		displayTopCircularList();
+		return maxNode;
 	}
 	
 	public void displayTopCircularList() {
@@ -29,12 +51,13 @@ public class FibonacciHeap {
 		System.out.println("back to head");
 	}
 	
-	Node insert(int frequency) {
+	Node insert(String str, int frequency) {
 		if (head == null) {
 			head = new Node();
 			head.val = frequency;
 			head.next = head;
 			head.prev = head;
+			head.name = str;
 			maxNode = head;
 			return head;
 		}
@@ -45,7 +68,27 @@ public class FibonacciHeap {
 		head.prev = node;
 		lastNode.next = node;
 		node.val = frequency;
+		node.name = str;
 		if (node.val > maxNode.val) maxNode = node;
+		return node;
+	}
+	
+	Node insert(Node node) {
+		if (node == null) return null;
+		if (head == null) {
+			head = node;
+			head.val = node.val;
+			head.next = head;
+			head.prev = head;
+			head.name = node.name;
+			maxNode = head;
+			return head;
+		}
+		Node lastNode = head.prev;
+		node.prev = lastNode;
+		node.next = head;
+		head.prev = node;
+		lastNode.next = node;
 		return node;
 	}
 	
@@ -58,7 +101,8 @@ public class FibonacciHeap {
 class Node {
 	Node next;
 	Node prev;
-	Node[] children;
+	ArrayList<Node> children = new ArrayList<Node>();
 	Node parent;
 	int val;
+	String name;
 }
